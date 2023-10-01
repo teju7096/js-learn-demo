50615c94517b3e51782d9ce0a74159b1b06523e3
teju7096 <146507173+teju7096@users.noreply.github.com>
2023-10-01 11:02:04 +0000

    datatypes

---
+++ 01_basics/02_data_types.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer Version
+
+// alert("Hello") // we are using node js not browser.. that alert only works in browser console.
+
+console.log(3+3); console.log("Tejashree") // Code readability should be high
+
+let name = "Tejashree"
+let age = 25
+let is_log = false
+
+// number => 2 to power 53 // that store that range of memory
+// bigInt // that store more range or memory occupied data
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol // unique value defined
+
+
+// object
+console.log(typeof"tejashree")
+console.log(typeof age);
+console.log(typeof undefined) // answer : undefined
+console.log(typeof null) // answer : object
