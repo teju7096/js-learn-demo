88dcff114c641979e22abb8281bd4c2c57e74082
teju7096 <146507173+teju7096@users.noreply.github.com>
2023-10-05 17:26:53 +0000

    string and memory

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+const name = "Tejashree"
+const age = 25
+
+console.log(name + age + " Valueeeeeeeee")
+
+console.log(`Hello My name is ${name} and my age is ${age}`); // That called string interpolation
+
+const Fname = new String("Tejashree") // Define string as object and access its method also
+
+console.log(Fname)
+console.log(Fname[0])
--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -37,3 +37,33 @@ console.log(typeof myFunc)
 
 // for read which data type return value
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+// +++++++++++++MEMORY++++++++++++++++
+
+//1. STACK -- All primitive types use STACK memory -- copy of a value
+//2. HEAP -- All non-primitive types use HEAP memory -- refrence of a value
+
+
+let MyLession = "Javascript"
+let AnotherLession = MyLession
+
+MyLession = "Python"
+
+console.table([MyLession,AnotherLession])
+
+let userOne = {
+    email:"user1@gameil.com",
+    upi:'up@sbi'
+}
+
+let userTwo = userOne
+
+userTwo.email = "tejmore2.tm@gamil.com"
+
+console.table([userOne.email,userTwo.email])
+
+// So conclude : 
+// STACK : When assigne one value to another variable and then change this another variable so first varibale not change
+// HEAP : When assigne one value to another variable and then change this another variable so all values change because that give address for that value
